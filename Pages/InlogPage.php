ce4bb7e352f179885506ee6cfc0eb09963a919f0
Rayanely <562891@edu.rocmn.nl>
2023-06-15 13:13:51 +0200

    Styling van de inlogpagina en overzicht pagina

--- Pages/InlogPage.php
+++ Pages/InlogPage.php
@@ -1,7 +1,3 @@
-<?php
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -9,11 +5,67 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/5622272db3.js" crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Geologica:wght@200&family=Roboto&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../Media/CSS/inlog.css">
     <title>De gouden Schoen || Inlog</title>
 </head>
 
 <body>
+    <div class="blok1"></div>
+    <div class="blok2"></div>
+ 
+    <div id="content">
+        <div class="blok">
+            <div class="veld">
+                <img src="../Media/img/schoen.jpg">
 
+                <div class="inlog">
+                    <br>
+                    <h2>Inloggen</h2>
+                    <br>
+                    <br>
+                    <form action="" class="form">
+                        <div class="form__veld">
+                            <label for="email">Email</label><br>
+                            <input type="text" class="input_veld" name="email" id="email">
+                        </div>
+                        <br>
+                        <div class="form__veld">
+                            <label for="password">Wachtwoord</label><br>
+                            <input type="password" class="input_veld" name="password" id="password" />
+                            <i class="fa-solid fa-eye-slash" onclick="myEye()"></i>
+                        </div>
+                        
+                        <br>
+                        <div class="submit_button">
+                            <button name="submit" class="submit">Inloggen</button>
+                        </div>
+                    </form>
+                    <br>    
+                    <br>
+                    <div class="copy">
+                        <p>© De gouden schoen. All rights reserved | Design by Code Crusaders</p>
+                    </div>
+                </div>
+
+            </div>
+
+
+        </div>
+    </div>
+   
 </body>
 
+<script>
+    function myEye() {
+        let x = document.getElementById('password');
+
+        if (x.type === "password") {
+            x.type = "text";
+        } else {
+            x.type = "password";
+        }
+    }
+</script>
 </html>
--- Pages/UserOverview.php
+++ Pages/UserOverview.php
@@ -1,6 +1,3 @@
-<?php
-
-?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,11 +5,58 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Geologica:wght@200&family=Roboto&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../Media/CSS/UserOverview.css">
     <title>De Gouden Schoen || User Overview</title>
 </head>
 
 <body>
-    <?php include "../Media/Templates/header.php" ?>
+    <div id="background">
+        <div class="container">
+            <table class="table_crud">
+                <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Naam</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Rol</th>
+                        <th scope="col">Actie</th>
+                    </tr>
+                </thead>
+                <tbody>
+                <?php
+            include "../Media/Templates/DBConnect.php";
+            $sql = "SELECT * FROM `users_elearn`";
+            $result = $conn->query($sql);
+
+            if ($result) {
+                while ($row = $result->fetch_object()) {
+                    $id = $row->id;
+                    $username = $row->username;
+                    $email = $row->email;
+                    $rol = $row->rol;
+
+                    echo '<tr class="actief">
+                    <th scope="row">' . $id . '</th>
+                    <td data-label="Naam: ">' . $username . '</td>
+                    <td data-label="Email: ">' . $email . '</td>
+                    <td data-label="Rol: ">' . $rol . '</td>
+            
+                    <td>
+                    <button class="trash"><a href="wis.php? wisid=' . $id . '">Wis</a></button>
+                    <button class="trash"><a href="wijzig.php? wijzigid=' . $id . '">Wijzig</a></button>
+                    
+                    </td>
+                
+                    </tr>';
+                }
+            }
+
+            ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
 </body>
 
 </html>
