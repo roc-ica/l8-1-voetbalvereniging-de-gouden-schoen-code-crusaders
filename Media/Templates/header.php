4fc0d27e502da1e8824d17b87eb668ea9293e6c2
smeikol <564500@edu.rocmn.nl>
2023-06-21 13:00:22 +0200

    nav gefixd

--- Media/Templates/header.php
+++ Media/Templates/header.php
@@ -4,12 +4,18 @@
 <header>
     <h1 class="Logo-Header">De <span class="gold">Gouden</span>schoen</h1>
     <ul class="ulheader">
-        <li class="liheader"><a class="headerLink" href="../../Pages/index.php">Home</a></li>
-        <li class="liheader"><a class="headerLink" href="../../Pages/Agenda.php">Agenda</a></li>
+        <li class="liheader"><a class="headerLink" href="../Pages/index.php">Home</a></li>
+        <li class="liheader"><a class="headerLink" href="../Pages/Agenda.php">Agenda</a></li>
         <li class="liheader">Contact</li>
     </ul>
     <div class="Login-Header">
-        <button class="buttonLogin">Inloggen</button>
+        <button class="buttonLogin" onclick="tologin()" >Inloggen</button>
         <button id="darkModeButton" class="buttonLogin">Donkere Modus</button>
     </div>
 </header>
+
+<script>
+        function tologin() {
+            window.location.replace("../Pages/InlogPage.php")
+        }
+    </script>
