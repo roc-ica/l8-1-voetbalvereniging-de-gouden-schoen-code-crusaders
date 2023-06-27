80c378eb3c8fd46322ecb29a7cb03e4b1cab628e
smeikol <564500@edu.rocmn.nl>
2023-06-27 16:58:25 +0200

    hop on (mytasks.php)

---
+++ Pages/mytasks.php
@@ -0,0 +1,138 @@
+<?php
+session_start();
+if (!(isset($_SESSION['sessionid']) || $_SESSION['sessionid'] == session_id() || ($_SESSION['role'] == 2 || $_SESSION['role'] == 1))) {
+    header("location: index.php");
+}
+include "../Media/Templates/DBConnect.php";
+
+if (isset($_POST['submit'])) {
+    $id = $_POST['id'];
+    $stmt = $conn->prepare("DELETE FROM user_has_task WHERE User_Task_ID=?;");
+    $stmt->bind_param("s", $id);
+    $stmt->execute();
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../Media/CSS/main.css">
+    <link rel="stylesheet" href="../Media/CSS/header.css">
+    <link rel="stylesheet" href="../Media/CSS/home.css">
+    <link rel="stylesheet" href="../Media/CSS/TaskOverview.css">
+    <style>
+
+    </style>
+    <title>De Gouden Schoen || Task Signup</title>
+</head>
+
+<body>
+    <?php include "../Media/Templates/header.php" ?>
+    <section class="TaskSection">
+        <div class="TaskContainer">
+            <?php
+
+            $STMT = $conn->prepare("SELECT * FROM `task`;");
+            if ($STMT == false) {
+                die("Secured");
+            }
+            $RESULT = $STMT->execute();
+            if ($RESULT == false) {
+                die("Secured");
+            }
+
+            $RESULT = $STMT->get_result();
+
+            while ($row = $RESULT->fetch_array()) :
+                $sql2 = "SELECT * FROM user_has_task WHERE Task_ID =?";
+                $stmt2 = $conn->prepare($sql2);
+                $stmt2->bind_param('s', $row['Task_ID']);
+                $stmt2->execute();
+                $result2 = $stmt2->get_result();
+                while ($row3 = $result2->fetch_array()) {
+
+                if ($row3["User_ID"] == $_SESSION['uid'] && new DateTime() < new DateTime($row['EndDate'])) {
+
+            ?>
+
+                    <div class="Tasks" id="<?php echo $row["Task_ID"] ?>">
+                        <h3><?php echo $row["Title"] ?></h3>
+                        <?php if ($row["Description"] != null) : ?>
+                            <h4>Description</h4>
+                            <p><?php echo $row["Description"] ?></p>
+                        <?php endif ?>
+                        <h4>Capacity:</h4>
+                        <p><?php echo $row["Capacity"] ?></p>
+                        <h4>Category:</h4>
+                        <p>
+                            <?php
+                            $STMT = $conn->prepare("SELECT `Category_ID`, `Name` FROM `category` WHERE `Category_ID` = ?");
+                            if ($STMT == false) {
+                                die("Secured");
+                            }
+
+                            $RESULT2 = $STMT->bind_param('s', $row["Category_ID"]);
+                            if ($RESULT2 == false) {
+                                die("Secured");
+                            }
+
+                            $RESULT2 = $STMT->execute();
+                            if ($RESULT2 == false) {
+                                die("Secured");
+                            }
+
+                            $RESULT2 = $STMT->get_result();
+
+                            while ($row2 = $RESULT2->fetch_array()) {
+                                echo $row2["Name"];
+                            }
+                            ?>
+                        </p>
+                        <div>
+                            <h4>Time</h4>
+                            <div>
+                                <h5>Start date</h5>
+                                <div>
+                                    <?php
+                                    $datetime = explode(" ", $row["StartDate"]);
+                                    for ($i = 0; $i < count($datetime); $i++) : ?>
+                                        <p>
+                                            <?php echo $datetime[$i] ?>
+                                        </p>
+                                    <?php endfor ?>
+                                </div>
+                                <h5>End date</h5>
+                                <div>
+                                    <?php
+                                    $datetime = explode(" ", $row["EndDate"]);
+                                    for ($i = 0; $i < count($datetime); $i++) : ?>
+                                        <p>
+                                            <?php echo $datetime[$i] ?>
+                                        </p>
+                                    <?php endfor ?>
+                                </div>
+                            </div>
+                        </div>
+
+                        <?php if(new DateTime('now+1day') < new DateTime($row['StartDate'])) { ?>
+                        <form action="" method="post">
+                            <input type="hidden" name="id" value=<?php echo $row3['User_Task_ID'] ?>></input>
+
+                            <button class="buttonLogin DeleteButton" type="submit" name="submit">cancel task</button>
+                        </form>
+                    
+            <?php } ?>
+            </div>
+            <?php }}
+            endwhile ?>
+        </div>
+    </section>
+</body>
+
+</html>
